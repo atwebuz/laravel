2a6ac5ff32ee8d7b650e6a43867a57ecbab19b52
Abdulaziz <abdusattorov2004@mail.ru>
2023-08-05 13:46:01 +0500

    creating blades

---
+++ resources/views/post.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('assets/css/app.css')}}">
+    <title>Document</title>
+</head>
+<body>
+    <article>
+        <h1><a href="/post"> First post</a></h1>
+        <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores amet numquam ut inventore? Quidem voluptas aliquid hic cumque eius quae.
+        </p>
+    </article>
+
+    <a href="/">Go Back</a>
+</body>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return ['hello' => 'bar'];
+    return view('posts');
+});
+
+Route::get('/post', function () {
+    return view('post');
 });
